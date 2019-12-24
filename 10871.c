f16827ac3951546611dfd276b40d358c2582054b
Dayoung98 <58020493+Dayoung98@users.noreply.github.com>
2019-12-25 01:18:03 +0900

    X 보다 작은 수

---
+++ 10871.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int N,X,A;
+    scanf("%d %d",&N,&X);
+    int arr[N];
+    for(int i=0;i<N;i++){
+        scanf("%d",&A);
+        arr[i] = A;
+    }
+    for(int i=0;i<N;i++){
+        if(arr[i]<X){
+            printf("%d ",arr[i]);
+        }
+    }
+}
+/*정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오
+첫째 줄에 N과 X가 주어진다. (1 ≤ N, X ≤ 10,000)
+
+둘째 줄에 수열 A를 이루는 정수 N개가 주어진다. 주어지는 정수는 모두 1보다 크거나 같고, 10,000보다 작거나 같은 정수이다.*/
